9ef75faece449464bab583edc6eb1bf079b8bd38
WILL MECHEM <wmechem@gmail.com>
2016-06-15 12:33:43 -0400

    pylint and flake8

--- py_boilermaker.py
+++ py_boilermaker.py
@@ -1,5 +1,7 @@
 """ Builds a skeleton python program file
 w. mechem 6/8/2016
+pylint 6/15/2016
+flake8
 
 Usage: py_boilermaker.py --name 'module_name.py' --imports --main --tests
 
@@ -21,7 +23,6 @@ Copyright 2016 Willard H. Mechem, III
 """
 
 from datetime import datetime
-import sys
 import argparse
 
 from template import function_definitions, imports, tests
@@ -31,24 +32,23 @@ ARGS = None
 
 def add_arguments():
     """ Add arguments from command line to parser. """
-    global ARGS
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--user", help="Specify user name",
-        default='wmechem@gmail.com')
+                        default='wmechem@gmail.com')
     parser.add_argument("--name", help="Specify module name",
-        default='test.py')
+                        default='test.py')
     parser.add_argument("--date", help="Optional create or modify date",
-        default=str(datetime.now()))
+                        default=str(datetime.now()))
     parser.add_argument("--desc", help="Description of the module, doc string",
-        default="Test Module Boilerplate")
+                        default="Test Module Boilerplate")
     parser.add_argument("--tests", help="Generate Unittests",
-        action='store_true')
+                        action='store_true')
     parser.add_argument("--imports", help="Include import statements",
-        action='store_true')
+                        action='store_true')
     parser.add_argument("--main", help="Generate main()",
-        action='store_true')
+                        action='store_true')
 
     args = parser.parse_args()
 
@@ -59,21 +59,22 @@ class Boilerplate(object):
     """ Create class instance with template methods. """
 
     def __init__(self):
-        self.function_defs=None
+        self.function_defs = None
         self.args = ARGS
         self._file = None
-        self.lr = '\n'
-
+        self.tests = None
+        self.imports = None
+        self.line_ret = '\n'
 
     def gen_comments(self):
         """ Generate the docstring for the first few lines of the program. """
           # line return
-        header_comments_section = '"""' + self.lr
-        header_comments_section += self.args.name + self.lr
-        header_comments_section += self.args.user + self.lr
-        header_comments_section += self.args.date + self.lr
-        header_comments_section += self.args.desc + self.lr
-        header_comments_section += '"""' + self.lr
+        header_comments_section = '"""' + self.line_ret
+        header_comments_section += self.args.name + self.line_ret
+        header_comments_section += self.args.user + self.line_ret
+        header_comments_section += self.args.date + self.line_ret
+        header_comments_section += self.args.desc + self.line_ret
+        header_comments_section += '"""' + self.line_ret
         print(header_comments_section)
 
         self.write_to_file(header_comments_section)
@@ -83,14 +84,15 @@ class Boilerplate(object):
     def gen_imports(self):
         """Add imports section. """
         self.imports = imports()
-        #[self.write_to_file(name + self.lr) for name in imports]
+        #[self.write_to_file(name + self.line_ret) for name in imports]
         for name in self.imports:
-            self.write_to_file("import " + name + self.lr)
+            self.write_to_file("import " + name + self.line_ret)
 
     def write_to_file(self, text):
+        """ Write contents to module file. """
         _file = self.args.name
-        with open(_file, 'a') as file:
-            file.write(text)
+        with open(_file, 'a') as out_file:
+            out_file.write(text)
         return
 
     def gen_sections(self):
@@ -102,17 +104,18 @@ class Boilerplate(object):
             """ Generate each line for function definitions. """
             print(name, params)
             section_text = ''
-            section_text += self.lr * 2
+            section_text += self.line_ret * 2
             section_text += 'def '
-            section_text += name + '('+ params + '):' + self.lr
+            section_text += name + '(' + params + '):' + self.line_ret
             section_text += ' ' * 4
-            section_text += '""" Doc string for ' + name +'. """' + self.lr
+            section_text += '""" Doc string for ' + name + '. """'
+            section_text += self.line_ret
             section_text += ' ' * 4
             section_text += 'pass'
-            section_text += self.lr
+            section_text += self.line_ret
             section_text += ' ' * 4
             section_text += 'return out_param'
-            section_text += self.lr
+            section_text += self.line_ret
 
             self.write_to_file(section_text)
 
@@ -149,28 +152,28 @@ class Boilerplate(object):
                 class_name = function_name.title()
 
             test_text = ''
-            test_text += self.lr * 2
+            test_text += self.line_ret * 2
             test_text += 'class Test' + class_name
             test_text += 'NotEqual(unittest.TestCase):'
-            test_text += self.lr
+            test_text += self.line_ret
             test_text += ' ' * 4
-            test_text += '""" Unittest """' + self.lr
+            test_text += '""" Unittest """' + self.line_ret
             test_text += ' ' * 4
-            test_text += '# pylint: disable=R0904' + self.lr
+            test_text += '# pylint: disable=R0904' + self.line_ret
             test_text += ' ' * 4
-            test_text += 'def setUp(self):' + self.lr
+            test_text += 'def setUp(self):' + self.line_ret
             test_text += ' ' * 8
-            test_text += 'self.results = (None)' + self.lr
+            test_text += 'self.results = (None)' + self.line_ret
             test_text += ' ' * 8
             test_text += 'self.test_data = (123)  # change this!!!!'
-            test_text += self.lr * 2
+            test_text += self.line_ret * 2
             test_text += ' ' * 4
-            test_text += 'def test_function(self):' + self.lr
+            test_text += 'def test_function(self):' + self.line_ret
             test_text += ' ' * 8
-            test_text += '""" Test function Assert Equal """' + self.lr
+            test_text += '""" Test function Assert Equal """' + self.line_ret
             test_text += ' ' * 8
             test_text += test_name + '(self.results, ' + function_name
-            test_text += '(self.test_data))' + self.lr
+            test_text += '(self.test_data))' + self.line_ret
               # 2 lines before next Class
 
             self.write_to_file(test_text)
@@ -188,50 +191,50 @@ class Boilerplate(object):
     def gen_main(self):
         """Add __main__ with calls to functions. """
         run_text = ""
-        run_text += self.lr * 2
+        run_text += self.line_ret * 2
         run_text += 'def main():'
-        run_text += self.lr
+        run_text += self.line_ret
         run_text += ' ' * 4
         run_text += '""" Execute functions in order. """'
-        run_text += self.lr
+        run_text += self.line_ret
 
         for function_def in self.function_defs:
             function_name = str(function_def['name'])
             run_text += ' ' * 4
             run_text += function_name + '()'
-            run_text += self.lr
+            run_text += self.line_ret
 
-        run_text += self.lr * 2
+        run_text += self.line_ret * 2
         run_text += 'if __name__ == "__main__":'
-        run_text += self.lr
+        run_text += self.line_ret
         run_text += ' ' * 4
         run_text += 'unittest.main()'
-        run_text += self.lr
+        run_text += self.line_ret
         run_text += ' ' * 4
         run_text += 'main()'
-        run_text += self.lr * 2
+        run_text += self.line_ret * 2
 
         self.write_to_file(run_text)
 
         return
 
+
 def main():
     """ Execute pipeline functions to build boilerplate file. """
-    global ARGS
-    ARGS = add_arguments()
+    arguments = add_arguments()
     boilerplate = Boilerplate()
 
     boilerplate.gen_comments()
 
-    if ARGS.imports is not False:
+    if arguments.imports is not False:
         boilerplate.gen_imports()
 
     boilerplate.gen_sections()
 
-    if ARGS.tests is not False:
+    if arguments.tests is not False:
         boilerplate.gen_tests()
 
-    if ARGS.main is not False:
+    if arguments.main is not False:
         boilerplate.gen_main()
 
 if __name__ == '__main__':
